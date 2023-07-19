1471ec054fa83a65e5e0e4c233c433ae3cc2fdec
stlabhishek <112849437+stlabhishek@users.noreply.github.com>
2023-07-19 12:50:36 +0530

    Update nms_run.sh

--- nms_run.sh
+++ nms_run.sh
@@ -1,9 +1,10 @@
-#!usr/bin/env bash
+#!/bin/bash
 echo "Script started"
 echo "please enter name of branch you want to clone ->"
 read br
 git clone --branch $br https://github.com/Sterlite-Technologies-Gurgaon-COE/stlnms.git
 time(sudo ./stlnms/clean.pl && sudo ./stlnms/compile.pl -DskipTests -Dcheckstyle.skip && sudo ./stlnms/assemble.pl -DskipTests -Dcheckstyle.skip -p dir)
+mkdir $PWD/database
 sudo $PWD/stlnms/target/stlnms-27.2.0/bin/runjava -s
 if [ ! "$(docker ps -a -q -f name=^nms_db)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=^nms_db)" ]; then
@@ -14,8 +15,6 @@ if [ ! "$(docker ps -a -q -f name=^nms_db)" ]; then
     sudo docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres --name nms_db -v './database':'/var/lib/postgresql/data' postgres:13.9
     sudo $PWD/stlnms/target/stlnms-27.2.0/bin/install -dis
     sudo $PWD/stlnms/target/stlnms-27.2.0/bin/opennms start
-else
-
-    sudo $PWD/stlnms/target/stlnms-27.2.0/bin/opennms start
 fi
-echo "Script completed"
+    
+echo "Script completed
